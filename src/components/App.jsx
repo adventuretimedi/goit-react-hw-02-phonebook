7f97612204fee7fb375dc6ccd1fc13055cad85fc
adventuretimedi <olhadianaaa@gmail.com>
2023-03-28 00:55:09 +0300

    Homework Steps 1-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,55 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import Phonebook from './Phonebook/Phonebook';
+import Contacts from './Contacts/Contacts';
+import Filter from './Filter/Filter';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = ({ name, number }) => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, { id: nanoid(), name, number }],
+    }));
+  };
+
+  filterContacts = filter => {
+    this.setState({ filter });
+  };
+
+  getFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+    const filterLower = filter.toLowerCase();
+    return filter === ''
+      ? contacts
+      : contacts.filter(contact =>
+          contact.name.toLowerCase().includes(filterLower)
+        );
+  };
+
+  render() {
+    return (
+      <div style={{ padding: '15px' }}>
+        <section>
+          <h2>Phonebook</h2>
+          <Phonebook onAddContact={this.addContact} />
+        </section>
+        <section>
+          <h2>Contacts</h2>
+          <Filter onFilterChanged={this.filterContacts} />
+          <Contacts contacts={this.getFilteredContacts()} />
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
